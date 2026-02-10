0b0741d34ccea4fc6bb2dfb852ec18c907925b5f
801Solofinance <invertircomunidad@gmail.com>
2026-02-10 15:53:44 +0100

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import { useBalance } from "../context/BalanceContext";
 import {
   User,
   Mail,
