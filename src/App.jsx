45a523caf84b7ae4586091a9a82765870ada4e32
801Solofinance <invertircomunidad@gmail.com>
2026-02-10 17:38:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import { AuthProvider } from "./context/AuthContext";
-import { AppProvider } from "./context/AppContext";
-import Layout from "./components/layout/Layout";
-import ProtectedRoute from "./components/layout/ProtectedRoute";
-
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Plans from "./pages/Plans";
-import Deposit from "./pages/Deposit";
-import Withdraw from "./pages/Withdraw";
-import History from "./pages/History";
-import Referrals from "./pages/Referrals";
-import Admin from "./pages/Admin";
-import Profile from "./pages/Profile";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './context/AuthContext'
+import { AppProvider } from './context/AppContext'
+import Layout from './components/layout/Layout'
+import ProtectedRoute from './components/layout/ProtectedRoute'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import Plans from './pages/Plans'
+import Deposit from './pages/Deposit'
+import Withdraw from './pages/Withdraw'
+import History from './pages/History'
+import Referrals from './pages/Referrals'
+import Admin from './pages/Admin'
+import Profile from './pages/Profile'
 
 function App() {
   return (
@@ -23,82 +22,20 @@ function App() {
         <AppProvider>
 
           <Routes>
-
-            {/* Public */}
-            <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
 
-            {/* Protected */}
             <Route element={<Layout />}>
-              <Route
-                path="/dashboard"
-                element={
-                  <ProtectedRoute>
-                    <Dashboard />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/plans"
-                element={
-                  <ProtectedRoute>
-                    <Plans />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/deposit"
-                element={
-                  <ProtectedRoute>
-                    <Deposit />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/withdraw"
-                element={
-                  <ProtectedRoute>
-                    <Withdraw />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/history"
-                element={
-                  <ProtectedRoute>
-                    <History />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/referrals"
-                element={
-                  <ProtectedRoute>
-                    <Referrals />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/profile"
-                element={
-                  <ProtectedRoute>
-                    <Profile />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/admin"
-                element={
-                  <ProtectedRoute requireAdmin>
-                    <Admin />
-                  </ProtectedRoute>
-                }
-              />
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+              <Route path="/plans" element={<ProtectedRoute><Plans /></ProtectedRoute>} />
+              <Route path="/deposit" element={<ProtectedRoute><Deposit /></ProtectedRoute>} />
+              <Route path="/withdraw" element={<ProtectedRoute><Withdraw /></ProtectedRoute>} />
+              <Route path="/history" element={<ProtectedRoute><History /></ProtectedRoute>} />
+              <Route path="/referrals" element={<ProtectedRoute><Referrals /></ProtectedRoute>} />
+              <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+              <Route path="/admin" element={<ProtectedRoute requireAdmin><Admin /></ProtectedRoute>} />
             </Route>
-
-            <Route path="*" element={<Navigate to="/" replace />} />
-
           </Routes>
 
           <Toaster position="top-right" />
@@ -106,7 +43,7 @@ function App() {
         </AppProvider>
       </AuthProvider>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
